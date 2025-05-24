b1978f8423133f45890cb16cd4afde1b1ccd212f
Hiroshi Kimura <muukii.app@gmail.com>
2025-05-24 15:39:33 +0900

    Add @TaskManager swiftui property wrapper (#7)

---
+++ Sources/ConcurrencyTaskManager/SwiftUI.swift
@@ -0,0 +1,84 @@
+import SwiftUI
+
+@propertyWrapper
+public struct TaskManager: DynamicProperty {
+  
+  @StateObject private var box: Box = .init(wrapper: .init(taskManager: .init()))
+  
+  @MainActor 
+  @preconcurrency 
+  public var wrappedValue: TaskManagerActorWrapper {
+    box.wrapper
+  }
+  
+  public init() {
+
+  }
+  
+  private final class Box: ObservableObject {
+    let wrapper: TaskManagerActorWrapper
+    
+    init(wrapper: TaskManagerActorWrapper) {
+      self.wrapper = wrapper
+    }
+    
+    deinit {
+      wrapper.cancelAllTasks()
+    }
+  }
+}
+
+public struct TaskManagerActorWrapper: Sendable {
+  
+  private let taskManager: TaskManagerActor
+  
+  public init(taskManager: TaskManagerActor) {
+    self.taskManager = taskManager
+  }
+  
+  @discardableResult
+  public func task<Return>(
+    label: String = "",
+    key: TaskKey,
+    mode: TaskManagerActor.Mode,
+    priority: TaskPriority = .userInitiated,
+    @_inheritActorContext _ operation: @Sendable @escaping () async throws -> Return
+  ) -> Task<Return, Error> {  
+    Task { [taskManager] in
+      try await taskManager
+        .task(
+          label: label,
+          key: key,
+          mode: mode,
+          priority: priority,
+          operation
+        )
+        .value
+    }
+  }
+  
+  public func cancelAllTasks() {
+    Task {
+      await taskManager.cancelAll()
+    }
+  }
+}
+
+#if DEBUG
+
+private struct _View: View {
+  
+  @TaskManager var taskManager
+    
+  var body: some View {
+    Button("Start") {
+      taskManager.task(key: .distinct(), mode: .dropCurrent) { 
+        try await Task.sleep(nanoseconds: 1_000_000_000)        
+        print("Task 1")
+      }
+    }
+  }
+  
+}
+
+#endif
